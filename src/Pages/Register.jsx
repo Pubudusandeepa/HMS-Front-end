2d25265c306b0bc4031d423c3e9f871314671fa8
Aasir Wahab <94959632+Aasirwahab@users.noreply.github.com>
2024-06-15 13:31:07 -0600

    update

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -25,7 +25,7 @@ const Register = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/patient/register",
+          "https://mhs-server-7.onrender.com/api/v1/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
